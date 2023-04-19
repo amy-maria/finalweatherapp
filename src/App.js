bc0457dd13a0e808833ddd48e854f8e207e90fe6
amy-maria <122695916+amy-maria@users.noreply.github.com>
2023-04-18 23:44:58 -0400

    Working on html and basic css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
 import "./App.css";
 import Search from "./Search";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      <Search />
-      <footer className="App-footer">
-        Amy Rowell. Code available on GitHub.
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <Search city="new york" />
+        <footer className="App-footer">
+          Amy Rowell. Code available on GitHub.
+        </footer>
+      </div>
     </div>
   );
 }
--- src/Search.js
+++ src/Search.js
@@ -1,46 +1,20 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-export default function Search() {
-  const [city, setCity] = useState("");
-  //const [message, setMessage] = useState("");
+export default function Search(props) {
+  const [city, setCity] = useState(props.city);
+  const [weatherData, setWeatherData] = useState({ ready: false });
 
   function showForecast(response) {
     console.log(response.data);
-
-    let date = new Date(response.data.dt * 1000);
-    let temp = Math.round(response.data.main.temp);
-    let wind = Math.round(response.data.wind.speed);
-    let windGust = Math.round(response.data.wind.gust);
-    //let windDirection = response.data.wind.deg;
-    //trying to convert degrees to direction
-    let sunrise = response.data.sys.sunrise * 1000;
-    let sunset = response.data.sys.sunset * 1000;
-    let feelsLike = Math.round(response.data.main.feels_like);
-    let humidity = response.data.main.humidity;
-    let currentCity = response.data.name;
-    let icon = `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`;
-    let weatherDescription = response.data.weather[0].main;
-    let formattedDate = Intl.DateTimeFormat("en-US", {
-      dateStyle: "full",
-    }).format(date);
-    let formattedTime = Intl.DateTimeFormat("en-US", {
-      timeStyle: "long",
-    }).format(date);
-    let dr = new Date(sunrise);
-    let ds = new Date(sunset);
-    let timeSunrise = Intl.DateTimeFormat("en-US", {
-      dateStyle: "short",
-      timeStyle: "long",
-    }).format(dr);
-    let timeSunset = Intl.DateTimeFormat("en-US", {
-      dateStyle: "short",
-      timeStyle: "long",
-    }).format(ds);
   }
 
   function handleSubmit(event) {
     event.preventDefault();
+    search();
+  }
+
+  function search() {
     let key = "caa883a4a60d93878755b08a933f74ea";
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=imperial`;
     axios.get(url).then(showForecast);
@@ -51,34 +25,34 @@ export default function Search() {
   }
 
   return (
-    <div className="currentWeather">
+    <div>
       <form onSubmit={handleSubmit}>
         <input type="search" placeholder="Enter a city" onChange={changeCity} />
         <input type="submit" value="search" />
       </form>
       <div className="d-flex mt-3 flex-row border border-primary">
         <div className="p-2 col-6">
-          <div className="todaysDate"> As of {formattedDate}</div>
-          <div className="todayTime">{formattedTime}</div>
-          <div className="currencity">Current conditions for {currentCity}</div>
-          <div className="weatherDescription">{weatherDescription}</div>
-          <img src={icon} alt="icon" aria-hidden="true" className="icon" />
+          <div className="todaysDate"> As of April 18, 2023</div>
+          <div className="todayTime">22:11</div>
+          <div className="currencity">Current conditions for New York</div>
+          <div className="weatherDescription">Cloudy</div>
+
           <div div className="currentTemp">
             {" "}
-            {temp} F<span className="celsius"> C</span>
+            50 F<span className="celsius"> C</span>
           </div>
           <div className="feelsLike">
-            Feels like: {feelsLike} F <span className="celsius"> C</span>
+            Feels like: 45 F <span className="celsius"> C</span>
           </div>
         </div>
         <div className="p-2 col-6">
-          <div className="wind">Wind: {wind} mph </div>
-          <div className="windGust">Wind gust: {windGust} mph</div>
-          <div className="humidity">Humidity: {humidity} %</div>
+          <div className="wind">Wind: 10 mph </div>
+          <div className="windGust">Wind gust: 5 mph</div>
+          <div className="humidity">Humidity: 25 %</div>
           <br />
           <br />
-          <div className="timeSunrise">Sunrise: {timeSunrise}</div>
-          <div className="timeSunset">Sunset: {timeSunset}</div>
+          <div className="timeSunrise">Sunrise: 06:30</div>
+          <div className="timeSunset">Sunset: 19:00 </div>
         </div>
       </div>
     </div>
